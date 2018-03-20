1a71d01b7260f2a65e03862c689b0b72adb0ecb8
summer <wpsumsun@126.com>
2018-03-20 23:20:50 +0800

    [Fix]下拉刷新

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,19 +9,29 @@ Page({
     start: 0,
     count: 10,
     total: 250,
-    isLoading: true
+    isLoading: true,
+    isDone: true,
+    scrollHeight: 0,
+    
   },
   onLoad: function () {
     this.getMovesRank()
+    this.getHeight()
   },
   getMovesRank() {
-    if (this.data.movies.length <= 250) {
+    this.data.isDone = false
+    if (this.data.movies.length < 50 && !this.data.isDone) {
       api.getMoviesRank('/v2/movie/top250', { start: this.data.start, count: this.data.count })
        .then((res) => {
          this.data.start += 10
-         this.setData({ isLoading: false, movies: res.subjects})
+         this.data.movies = this.data.movies.concat(res.subjects)
+         this.setData({ movies: this.data.movies, isLoading: false, isDone: true})
        })
+    } else {
+      wx.showToast({ title: '已经全部加载完了' })
     }
   },
-
+  loadMore () {
+    this.getMovesRank()
+  },
 })
