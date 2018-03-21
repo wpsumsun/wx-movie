f8ce1e66892c4360459e51508eb0a51d7eabab93
summer <wpsumsun@126.com>
2018-03-21 15:12:33 +0800

    [Feature]地图

--- pages/position/position.js
+++ pages/position/position.js
@@ -2,13 +2,23 @@ import api from "../../utils/api.js"
 Page({
 
   data: {
-    location: {}
+    location: {},
+    markers: [{
+      iconPath: "../../images/icon-location.png",
+      id: 0,
+      latitude: null,
+      longitude: null,
+      width: 50,
+      height: 50
+    }],
   },
 
   onLoad: function (options) {
+    var latitude = "markers[0].latitude"
+    var longitude = "markers[0].longitude"
     api.getLocation()
       .then((location) => {
-        this.setData({ location })
+        this.setData({ location, [latitude]: location.latitude, [longitude]: location.longitude,  })
       })
   },
 })
